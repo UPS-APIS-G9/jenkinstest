bee288d89cd44cb0b89bfc8622956f72674d2652
Fernando <wilmercaiza@gmail.com>
2025-01-23 22:53:27 -0500

    update the test message

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -10,7 +10,7 @@ public class BasicApplicationTests  {
 
     @Test
     void contextLoads() {
-       String message = "Default Message Cambio 2, test change message";
+       String message = "Default Message Cambio 2, test change message, again";
         assertNotNull(message);
     }
 }
